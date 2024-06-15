4cbb10f714bcc748d573d99754c85373d415fe7f
Igor Deo Alves <igordeo@id.uff.br>
2024-06-15 19:46:18 -0300

    chore: corrigindo formatacao e adequação ao Flake8

--- backend/models/ride.py
+++ backend/models/ride.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 class Ride(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     driver = db.Column(db.Integer, nullable=False)
@@ -7,6 +8,7 @@ class Ride(db.Model):
     start = db.Column(db.String(50), nullable=False)
     end = db.Column(db.String(50), nullable=False)
     status = db.Column(db.String(50), nullable=False)
+
     def to_dict(self):
         return {
             "id": self.id,
@@ -14,12 +16,13 @@ class Ride(db.Model):
             "vehicle_id": self.vehicle_id,
             "start": self.start,
             "end": self.end,
-            "status": self.status
+            "status": self.status,
         }
+
     def __repr__(self):
-        return '<Ride %r>' % self.id
-    
-    def __init__(self,dicio):
+        return "<Ride %r>" % self.id
+
+    def __init__(self, dicio):
         super().__init__()
         self.id = dicio["id"]
         self.driver = dicio["driver"]
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,19 +1,22 @@
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
+
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
             "email": self.email,
         }
+
     def __repr__(self):
-        return '<User %r>' % self.id
-    
-    def __init__(self,dicio):
+        return "<User %r>" % self.id
+
+    def __init__(self, dicio):
         super().__init__()
         self.id = dicio["id"]
         self.name = dicio["name"]
