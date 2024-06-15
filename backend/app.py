d0e6d32da9b83f5f04f92e8f83b053e6c3dc03a5
Raphael Garcia <raphaelreisflamengo@gmail.com>
2024-06-15 17:01:23 -0300

    connection with db

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,15 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./teste.db'
 db=SQLAlchemy(app)
-
-@app.route("/")
-def index():
-    return "ok"
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     debug = os.environ["FLASK_ENV"] == "development"
     app.run(debug=True, host='0.0.0.0', port=8000)
+
+import controllers.ride_controller
+import models.ride
+import models.vehicle
