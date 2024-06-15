8fda4aae55f5b9153d051befd6b899578d6dda0a
Wesley <me@ovvesley.com>
2024-06-15 17:47:16 -0300

    change bd to db

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,9 +1,9 @@
-from app import bd
+from app import db
 
-class user(bd.Model):
-    id = bd.Column(bd.Integer, primary_key=True)
-    name = bd.Column(bd.String(50), nullable=False)
-    email = bd.Column(bd.String(50), nullable=False)
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
     def to_dict(self):
         return {
             "id": self.id,
