0ab756f0f55d3ea825fddb1be7f4d38062cb1a3e
Wesley <me@ovvesley.com>
2024-06-15 17:50:11 -0300

    change name class user to User

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,6 +1,6 @@
 from app import db
 
-class user(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
