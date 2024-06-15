642376983745ce3b274a3582eb0d6bf6215913e4
Raphael Garcia <raphaelreisflamengo@gmail.com>
2024-06-15 09:39:53 -0300

    add db to app and dependencies

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db=SQLAlchemy(app)
 
 @app.route("/")
 def index():
@@ -8,4 +11,6 @@ def index():
 
 if __name__ == "__main__":
     debug = os.environ["FLASK_ENV"] == "development"
-    app.run(debug=debug, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
+
+import backend.controllers.ride_controller as ride_controller
