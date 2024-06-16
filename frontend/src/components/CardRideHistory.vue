aa4069218f3f2bdc06843a5636403173f179176e
AlexSilva-dev <alexdsilva.dev@gmail.com>
2024-06-15 23:12:31 -0300

    front end - melhorias na tela de histórico, implementação da avaliação

--- frontend/src/components/CardRideHistory.vue
+++ frontend/src/components/CardRideHistory.vue
@@ -44,12 +44,49 @@
 				>
 			</div>
 			<div class="text">
-				Saída:
-				Data <span class="text-bold">{{ ride.date }}</span>
-				|
+				Saída: Data
+				<span claszs="text-bold">{{ ride.date }}</span> |
 				Hora <span class="text-bold">{{ ride.hour }}</span>
 			</div>
 		</q-card-section>
+
+		<q-card-actions class="justify-center">
+			<q-btn flat color="primary" @click="assess = true">
+				Avaliar
+			</q-btn>
+			<q-dialog v-model="assess" persistent>
+				<q-card style="min-width: 350px">
+					<q-card-section>
+						<div class="text-h6">
+							O que você achou da corrida?
+						</div>
+					</q-card-section>
+
+					<q-card-section
+						class="q-pt-none row justify-center"
+					>
+						<q-rating
+							size="40px"
+							v-model="userRating"
+							:max="5"
+							color="primary"
+						/>
+					</q-card-section>
+
+					<q-card-actions
+						align="right"
+						class="text-primary"
+					>
+						<q-btn
+							flat
+							label="Avaliar"
+							@click="toAssess"
+							v-close-popup
+						/>
+					</q-card-actions>
+				</q-card>
+			</q-dialog>
+		</q-card-actions>
 	</q-card>
 </template>
 
@@ -61,6 +98,8 @@ const dialogReserver = ref(false);
 const dialogConfirmReserver = ref(false);
 const rideRequestDialog = ref(null);
 const dialogDriveProfile = ref(null);
+const assess = ref(0);
+const userRating = ref('');
 const props = defineProps({
 	ride: {
 		type: Object,
@@ -68,8 +107,8 @@ const props = defineProps({
 	},
 	id: {
 		type: Number,
-		required: true
-	}
+		required: true,
+	},
 });
 
 /**
@@ -89,5 +128,16 @@ const reserver = (element) => {
 	dialogReserver.value = false;
 	dialogConfirmReserver.value = true;
 	// router.push("/");
+};
+
+const toAssess = (userRating) => {
+	// @TODO chamar a api para guarda a avaliação
+	// axios.get('https://webhook.site/9b8728f1-c752-4950-b85c-6042ac7833dd')
+	//   .then(response => {
+	//     ride.userRating = userRating;
+	//   })
+	//   .catch(error => {
+	//     console.error(error);
+	//   });
 };
 </script>
