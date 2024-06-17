3481bca1be6a93ce061437b21426f136341f017b
Raphael Garcia <raphaelreisflamengo@gmail.com>
2024-06-17 20:54:48 -0300

    update ride's model, service and controller to new architecture

--- backend/src/models/ride.py
+++ backend/src/models/ride.py
@@ -1,12 +1,13 @@
 from src import db
+from datetime import datetime
 
 
 class Ride(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     driver = db.Column(db.Integer, nullable=False)
     vehicle_id = db.Column(db.Integer, nullable=False)
-    start = db.Column(db.String(50), nullable=False)
-    end = db.Column(db.String(50), nullable=False)
+    start = db.Column(db.DateTime(50), nullable=False)
+    end = db.Column(db.DateTime(50),nullable=True)
     status = db.Column(db.String(50), nullable=False)
 
     def to_dict(self):
@@ -22,11 +23,12 @@ class Ride(db.Model):
     def __repr__(self):
         return "<Ride %r>" % self.id
 
-    def __init__(self, dicio):
+    def __init__(self, id: int, driver: int, vehicle_id: int, status: str, start: datetime, end: datetime):
         super().__init__()
-        self.id = dicio["id"]
-        self.driver = dicio["driver"]
-        self.vehicle_id = dicio["vehicle_id"]
-        self.start = dicio["start"]
-        self.end = dicio["end"]
-        self.status = dicio["status"]
+        self.id = id
+        self.driver = driver
+        self.vehicle_id = vehicle_id
+        if start != None:
+            self.start = start
+        self.end = end
+        self.status = status
--- backend/src/services/ride_service.py
+++ backend/src/services/ride_service.py
@@ -1,24 +1,27 @@
-from app import db
+from src import db
+from src.models import Ride
+from datetime import datetime
 
 
-def get_ride(self, ride_id):
+def get_ride(ride_id):
     try:
-        ride = db.session.query(ride).filter_by(id=ride_id).first()
+        ride = Ride.query.filter_by(id=ride_id).first()
         return ride.to_dict()
     except Exception:
         return "Ride not found"
 
 
-def get_all_rides(self):
+def get_all_rides():
     try:
-        rides = db.session.query(rides).all()
+        rides = Ride.query.all()
         return [ride.to_dict() for ride in rides]
     except Exception:
         return "No rides found"
 
 
-def add_ride(self, ride):
+def add_ride(ride_json):
     try:
+        ride = Ride(ride_json.get("id"), ride_json.get("driver"), ride_json.get("vehicle_id"), ride_json.get("status"),datetime.now(),None)
         db.session.add(ride)
         db.session.commit()
         return ride.to_dict()
@@ -26,18 +29,18 @@ def add_ride(self, ride):
         return "Error adding ride"
 
 
-def update_ride(self, ride_id, ride):
+def update_ride(ride_id):
     try:
-        db.session.query(ride).filter_by(id=ride_id).update(ride)
+        Ride.query.filter_by(id=ride_id).update({"end": datetime.now(), "status": "completed"})
         db.session.commit()
-        return ride.to_dict()
+        return "Ride updated successfully"
     except Exception:
         return "Error updating ride"
 
 
-def delete_ride(self, ride_id):
+def delete_ride(ride_id):
     try:
-        ride = db.session.query(ride).filter_by(id=ride_id).first()
+        ride = Ride.query.filter_by(id=ride_id).first()
         db.session.delete(ride)
         db.session.commit()
         return ride.to_dict()
