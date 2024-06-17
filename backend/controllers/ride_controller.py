6de3d217139a31ac2a19a5aa4a78e4c8283bca9a
Raphael Garcia <raphaelreisflamengo@gmail.com>
2024-06-17 09:08:23 -0300

    changes ride controller and service

--- backend/controllers/ride_controller.py
+++ backend/controllers/ride_controller.py
@@ -1,21 +1,27 @@
 from app import app
-from backend.services import ride_service
+from services import ride_service
+from flask import request
 
-@app.route("/ride", methods=["GET"])
-def get(self):
-    if not app.request.args:
-        return app.jsonify(ride_service.get_all_rides())
+@app.route("/ride/<int:id>", methods=["GET"])
+def get(id):
+    assert id == request.view_args['id']
+    if id == None:
+        return ride_service.get_all_rides()
     else:    
-        return app.jsonify(ride_service.get_ride(app.request.args.get('id')))
+        return ride_service.get_ride(request.args.get('id'))
 
 @app.route("/ride", methods=["POST"])
-def post(self):
-    return app.jsonify(ride_service.add_ride(app.request.json))
+def post():
+    return ride_service.add_ride(request.get_json())
 
-@app.route("/ride", methods=["PUT"])
-def put(self):
-    return app.jsonify(ride_service.update_ride(app.request.args.get('id'), app.request.json))
+@app.route("/ride/<int:id>", methods=["PUT"])
+def put():
+    if app.request.args.get('id') == None:
+        return 'Please provide an id'
+    return ride_service.update_ride(app.request.args.get('id'), app.request.json)
 
-@app.route("/ride", methods=["DELETE"])
-def delete(self):
-    return app.jsonify(ride_service.delete_ride(app.request.args.get('id')))
+@app.route("/ride/<int:id>", methods=["DELETE"])
+def delete():
+    if app.request.args.get('id') == None:
+        return 'Please provide an id'
+    return ride_service.delete_ride(app.request.args.get('id'))
--- backend/services/ride_service.py
+++ backend/services/ride_service.py
@@ -1,40 +1,45 @@
-from app import db
-
+from models import ride as ride_model
 def get_ride(self, ride_id):
     try:
-        ride = db.session.query(ride).filter_by(id=ride_id).first()
+        ride = ride_model.query.filter_by(id=ride_id).first()
         return ride.to_dict()
     except:
         return 'Ride not found'
 
-def get_all_rides(self):
+def get_all_rides():
     try:
-        rides = db.session.query(rides).all()
+        rides = ride_model.query.all()
         return [ride.to_dict() for ride in rides]
     except:
         return 'No rides found'
 
-def add_ride(self, ride):
+def add_ride(json_ride):
+    ride = ride_model.ride(json_ride)
     try:
-        db.session.add(ride)
-        db.session.commit()
+        ride_model.add(ride)
+        ride_model.commit()
         return ride.to_dict()
     except:
         return 'Error adding ride'
 
-def update_ride(self, ride_id, ride):
+def update_ride(self, ride_id, up_ride):
     try:
-        db.session.query(ride).filter_by(id=ride_id).update(ride)
-        db.session.commit()
+        ride = ride_model.query(ride).filter_by(id=ride_id).first()
+        ride.driver = up_ride['driver']
+        ride.vehicle_id = up_ride['vehicle_id']
+        ride.start = up_ride['start']
+        ride.end = up_ride['end']
+        ride.status = up_ride['status']
+        ride_model.commit()
         return ride.to_dict()
     except:
         return 'Error updating ride'
 
 def delete_ride(self, ride_id):
     try:
-        ride = db.session.query(ride).filter_by(id=ride_id).first()
-        db.session.delete(ride)
-        db.session.commit()
-        return ride.to_dict()
+        ride = ride_model.query(ride).filter_by(id=ride_id).first()
+        ride_model.delete(ride)
+        ride_model.commit()
+        return 'Ride deleted'
     except: 
         return 'Error deleting ride'
