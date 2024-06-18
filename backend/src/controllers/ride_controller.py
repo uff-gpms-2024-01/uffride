75d2e17927e65c254089b8a6cbe0756604d7ca87
Raphael Garcia <raphaelreisflamengo@gmail.com>
2024-06-18 09:30:48 -0300

    tests criation and change on endpoint's name from "ride" to "rides"

--- backend/src/controllers/ride_controller.py
+++ backend/src/controllers/ride_controller.py
@@ -1,6 +1,5 @@
 from flask import (
     Blueprint,
-    flash,
     request,
     jsonify,
 )
@@ -13,7 +12,7 @@ ride_bp = Blueprint("ride", __name__)
 def get(id):
     return jsonify(ride_service.get_ride(id))
 
-@ride_bp.route("/ride", methods=["GET"])
+@ride_bp.route("/rides", methods=["GET"])
 def getAll():    
     return jsonify(ride_service.get_all_rides())
 
